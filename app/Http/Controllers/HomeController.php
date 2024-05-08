fbc6eb46c967caf6358c7e2cbd83a50c93672402
Rheinmir <rheinmir@gmail.com>
2024-05-08 16:44:54 +0700

    Home controller

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -6,5 +6,11 @@ use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
-    //
+    public function index(){
+        return view('home.index');
+    }
+
+    public function about() {
+        return view('home.about');
+    }
 }
